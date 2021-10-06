253bf225faa85a5f78f86630cd46c2ebb079c748
Nathaniel <methernathaniel@gmail.com>
2021-10-06 17:34:23 +1100

    test email

--- html-file/inc/testemail.php
+++ html-file/inc/testemail.php
@@ -1,6 +1,17 @@
 <?php
-if($_POST["Message"]) {
-mail("nathanielmether@gmail.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: methernathaniel@gmail.com");
+
+if(isset($_POST['button_pressed']))
+{
+    $to      = 'nathanielmether@gmail.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: methernathaniel@gmail.com' . "\r\n" .
+        'Reply-To: methernathaniel@gmail.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+
+    echo 'Email Sent.';
 }
+
 ?>
